571e3f1ca348ac5fb347b0a5be654a7f7580a7e3
MaximMu-CISEMI <55351322+MaximMu-CISEMI@users.noreply.github.com>
2023-04-13 19:49:31 +0300

    adding user profile

--- workbud/base/views.py
+++ workbud/base/views.py
@@ -66,8 +66,13 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains = q))
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    context = {'rooms': rooms, 
+               'topics': topics, 
+               'room_count': room_count,
+               'room_messages': room_messages
+               }
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
@@ -89,6 +94,15 @@ def room(request, pk):
                'participants': participants
                }
     return render(request, 'base/room.html', context)
+
+def user_profile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all() # all mdoels children
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics }
+    return render(request, 'base/profile.html', context )
+
 
 @login_required(login_url='login')
 def create_room(request):
@@ -130,6 +144,7 @@ def delete_room(request, pk):
         return redirect('home') # redirect by name 
 
     return render(request, 'base/delete.html', {'obj': room})
+
 
 
 @login_required(login_url='login')
